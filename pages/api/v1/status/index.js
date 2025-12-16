eba57d5d695c450771a5b636152ff57e4644b45e
Bruno Vinícius <vbruno96@gmail.com>
2025-12-15 21:36:40 -0300

    feat: return database status on endpoint `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,33 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnections = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseUserOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+  const databaseUserOpenedConnections =
+    databaseUserOpenedConnectionsResult.rows[0].opened_connections;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: databaseMaxConnections,
+        opened_connections: databaseUserOpenedConnections,
+        version: databaseVersion,
+      },
+    },
+  });
 }
