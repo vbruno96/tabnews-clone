30bbef6d0b5d80843e4a116bbd527d375811ab12
Bruno Vinícius <vbruno96@gmail.com>
2025-12-17 00:00:06 -0300

    feat: add endpoint `/api/v1/migrations`

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,17 @@
+test("POST to /api/v1/migrations should 201", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  const response1Body = await response1.json();
+
+  expect(response1.status).toBe(201);
+  expect(response1Body.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  const response2Body = await response2.json();
+
+  expect(response2.status).toBe(200);
+  expect(response2Body.length).toBe(0);
+});
