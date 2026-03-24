1e55ab3e5df7f084cf20f5154b5b5e5c3b783333
Bruno Vinícius <vbruno96@gmail.com>
2026-03-24 11:09:45 -0300

    test: add default user coverage for `GET /api/v1/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -24,6 +24,23 @@ describe("GET /api/v1/status", () => {
     });
   });
 
+  describe("Default User", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch(`${webserver.origin}/api/v1/status`);
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      const { database } = responseBody.dependencies;
+      expect(database.max_connections).toEqual(100);
+      expect(database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database).not.toHaveProperty("version");
+    });
+  });
+
   describe("Privileged User", () => {
     test("With `read:status:all", async () => {
       const testUser = await orchestrator.createUser();
