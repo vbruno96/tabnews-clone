be8bd58ea5313f0e19e7e3cbeb843d00121c2f92
Bruno Vinícius <vbruno96@gmail.com>
2026-08-11 09:56:24 -0300

    feat: use `DefaultLayout` on home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,18 @@
-import buildImg from "../src/assets/build.svg";
+import DefaultLayout from "interface/DefaultLayout";
+import buildImg from "src/assets/build.svg";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main style={{ height: "100dvh", fontFamily: "sans-serif" }}>
-      <h1>🪚 Em construção 🛠️</h1>
-      <Image src={buildImg} width={280} height={320} alt="" />
-      <p>
-        Isso aqui ainda é só uma ideia maluca que talvez no futuro vire hub de
-        notícias ou outra coisa qualquer. 😄
-      </p>
-    </main>
+    <DefaultLayout>
+      <main style={{ height: "100dvh", fontFamily: "sans-serif" }}>
+        <h1>🪚 Em construção 🛠️</h1>
+        <Image src={buildImg} width={280} height={320} alt="" loading="eager" />
+        <p>
+          Isso aqui ainda é só uma ideia maluca que talvez no futuro vire hub de
+          notícias ou outra coisa qualquer. 😄
+        </p>
+      </main>
+    </DefaultLayout>
   );
 }
